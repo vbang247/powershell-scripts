31fa8df8617c7d0e6b767d8e3227b7b079e946a1
Varsha Bang <56339499+vbang247@users.noreply.github.com>
2019-10-09 16:09:26 +0530

    Delete spark.ps1

--- spark.ps1
+++
@@ -1,9 +0,0 @@
-# Find latest installer
-$url = 'http://apache.mirror.anlx.net/spark/'
-Write-Host "Invoking URL $url for downloading Apache Spark latest distribution"
-$site = Invoke-WebRequest -Uri $url -UseBasicParsing
-$url = $url + ((Invoke-WebRequest -Uri $url -UseBasicParsing).Links | ?{ $_.tagName -eq 'a' -and $_.href.Contains('spark')} | Select-Object href -Last 1).href
-$url = $url + ((Invoke-WebRequest -Uri $url -UseBasicParsing).Links | ?{ $_.tagName -eq 'a' -and $_.href.Contains('bin-hadoop')} | Select-Object href -Last 1).href                   
-Write-Host "Complete URL for Apache Spark tar.gz file is $url"
-wget -q $url
-#tar -xf *.tgz
